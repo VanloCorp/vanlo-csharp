2a74d3be1d4b52c39d086798ca5d76ca6fc293dd
Oleh Bulatsan <obiolg@gmail.com>
2026-03-10 12:19:09 +0200

    feat: add missing API endpoint support (Report, Event, DisputeAdjustment, CreateAll, Rate.Create)
    * feat: add Report, Event resources and new endpoint actions
    
    - Add Report resource (Create, Retrieve, List) with custom URL pattern for report types
    - Add ReportList for paginated report listing
    - Add Event resource (List only)
    - Add EventList for paginated event listing
    - Add Shipment.DisputeAdjustment() instance method
    - Add ScanForm.CreateAll() static method
    - Add Rate.Create() static method for standalone rate quotes
    
    * chore: bump version to 1.1.0
    
    ---------
    
    Co-authored-by: h3d0 <h3d0@github.com>

--- Vanlo/Rate.cs
+++ Vanlo/Rate.cs
@@ -1,6 +1,7 @@
 ﻿using RestSharp;
 
 using System;
+using System.Collections.Generic;
 
 namespace Vanlo {
     public class Rate : Resource {
@@ -21,5 +22,27 @@ namespace Vanlo {
         public string carrier { get; set; }
         public string shipment_id { get; set; }
         public string carrier_account_id { get; set; }
+
+        /// <summary>
+        /// Create standalone rates for the given parameters.
+        /// </summary>
+        /// <param name="parameters">
+        /// Dictionary containing parameters to generate rates with. Valid pairs:
+        ///   * {"from_address", Dictionary&lt;string, object&gt;} Origin address.
+        ///   * {"to_address", Dictionary&lt;string, object&gt;} Destination address.
+        ///   * {"parcel", Dictionary&lt;string, object&gt;} Parcel details.
+        /// </param>
+        /// <returns>List of Vanlo.Rate instances.</returns>
+        public static List<Rate> Create(Dictionary<string, object> parameters) {
+            Request request = new Request("/rate", Method.POST);
+            request.AddBody(parameters);
+
+            RateListResponse response = request.Execute<RateListResponse>();
+            return response.rates;
+        }
+
+        private class RateListResponse : Resource {
+            public List<Rate> rates { get; set; }
+        }
     }
 }
